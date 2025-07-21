226c530ae6a550bfe1277bfddcc378f620662e16
Yael96334 <yael96334@gmail.com>
2025-07-21 11:56:41 +0300

    some changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/peletop-new'),
+      dir: require('path').join(__dirname, './coverage/home-task'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://test.api.peletop.co.il/',
-  captchaToken: '6Lc_ImIpAAAAAIKqYgoOzmF_zq-7zTDqXJsnD2QO',
-  googleMapKey:'AIzaSyD5SbBAtcnqe3lfpNhDSDPCS5RFrTzssac'
-
-};
